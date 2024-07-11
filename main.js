954faae72d805f2401596a43439750cfc19dd1a6
Behruz12390 <168063527+Behruz12390@users.noreply.github.com>
2024-07-11 11:17:57 +0500

    first commit

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const array = [2, 4, 9, 10, 45, 67, 8, 90]
-const arr = []
+let array = [2, 4, 9, 10, 45, 67, 8, 90]
+let arr = []
 for(let i = 0; i < array.length ;i++){
     if(array[i] % 2 == 0){
        arr.push(array[i]) 
